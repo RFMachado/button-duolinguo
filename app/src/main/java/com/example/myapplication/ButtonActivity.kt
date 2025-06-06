742e8d1ab2cab5c1a6c20d48c117d19a91a53398
Rafael <rfmachado91@gmail.com>
2025-06-06 19:06:22 -0300

    teste button duolingo

--- app/src/main/java/com/example/myapplication/ButtonActivity.kt
+++ app/src/main/java/com/example/myapplication/ButtonActivity.kt
@@ -14,25 +14,19 @@ class ButtonActivity : AppCompatActivity() {
         binding = ActivityButtonBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         val shadowSize = 4f // dp
 
         fun dpToPx(dp: Float): Float {
             return dp * resources.displayMetrics.density
         }
 
-        binding.button.setOnTouchListener { v, event ->
+        binding.btn.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    binding.button.animate().translationY(dpToPx(shadowSize)).setDuration(50).start()
-                   // binding.shadowView.animate().translationY(0f).setDuration(50).start()
+                    binding.btn.translationY = dpToPx(shadowSize)
                 }
                 MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    binding.button.animate().translationY(0f).setDuration(50).start()
-                  //  binding.shadowView.animate().translationY(dpToPx(shadowSize)).setDuration(50).start()
-                    if (event.action == MotionEvent.ACTION_UP) {
-                        binding.button.performClick()
-                    }
+                    binding.btn.translationY = 0f
                 }
             }
             true
