55442731f6eba0b8a7121a5481c2795cc45a65d6
Rafael <rfmachado91@gmail.com>
2025-06-06 21:12:15 -0300

    teste button duolingo

--- app/src/main/java/com/example/myapplication/ButtonActivity.kt
+++ app/src/main/java/com/example/myapplication/ButtonActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityButtonBinding
 
@@ -14,9 +13,5 @@ class ButtonActivity : AppCompatActivity() {
         binding = ActivityButtonBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
     }
-
-
 }
--- app/src/main/java/com/example/myapplication/DuoButton.kt
+++ app/src/main/java/com/example/myapplication/DuoButton.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.myapplication.databinding.DuoButtonBinding
 
@@ -11,37 +12,72 @@ class DuoButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet,
     defStyleAttr: Int = 0
-): ConstraintLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     private lateinit var binding: DuoButtonBinding
+    private lateinit var button: LinearLayout
 
     init {
-       inflateLayout()
-       bindViews()
+        inflateLayout()
+        bindViews()
+        setupOntouchListener()
+    }
+
+    private fun createFramelayout() {
+//        val frameLayout = FrameLayout(context).apply {
+//            layoutParams = FrameLayout.LayoutParams(
+//                LayoutParams.MATCH_CONSTRAINT,
+//                LayoutParams.MATCH_CONSTRAINT
+//            )
+//            background = ContextCompat.getDrawable(context, R.drawable.duolingo_button_bg_shadow)
+//        }
+//
+//        // Criando as regras de posicionamento no ConstraintLayout
+//        val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_CONSTRAINT).apply {
+//            bottomToBottom = binding.btn.id
+//            startToStart = binding.btn.id
+//            endToEnd = binding.btn.id
+//            startToStart = binding.btn.id
+//        }
+//
+//// Aplicando as regras ao FrameLayout
+//        frameLayout.layoutParams = layoutParams
+//
+//        frameLayout.translationY = dpToPx(4f)
+    }
+
+    private fun setupOntouchListener() {
+        val shadowSize = 2f // dp
+        setOnClickListener {  }
+        apply {
+            isClickable = true
+            isFocusable = true
+            setOnTouchListener { view, event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        button.translationY = dpToPx(shadowSize)
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        button.translationY = 0f
+                        view.performClick()
+                    }
+                }
+                true
+            }
+        }
     }
 
     private fun bindViews() {
-        val shadowSize = 4f // dp
+        button = binding.btn
+    }
 
-        fun dpToPx(dp: Float): Float {
-            return dp * resources.displayMetrics.density
-        }
-
-        binding.btn.setOnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    binding.btn.translationY = dpToPx(shadowSize)
-                }
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    binding.btn.translationY = 0f
-                }
-            }
-            true
-        }
+    fun dpToPx(dp: Float): Float {
+        return dp * resources.displayMetrics.density
     }
 
     private fun inflateLayout() {
         binding = DuoButtonBinding.inflate(
-            LayoutInflater.from(context),this)
+            LayoutInflater.from(context), this
+        )
     }
 }
