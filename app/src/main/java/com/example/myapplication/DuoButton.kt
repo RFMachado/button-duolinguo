97601cd5770074ee209800fe249b0fe511371b85
Rafael <rfmachado91@gmail.com>
2025-06-06 21:38:36 -0300

    teste button duolingo

--- app/src/main/java/com/example/myapplication/DuoButton.kt
+++ app/src/main/java/com/example/myapplication/DuoButton.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.annotation.DrawableRes
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
 import com.example.myapplication.databinding.DuoButtonBinding
 
 class DuoButton @JvmOverloads constructor(
@@ -13,45 +18,49 @@ class DuoButton @JvmOverloads constructor(
     attrs: AttributeSet,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-
     private lateinit var binding: DuoButtonBinding
     private lateinit var button: LinearLayout
+    internal lateinit var icon: ImageView
 
     init {
         inflateLayout()
         bindViews()
         setupOntouchListener()
+        loadAttributes(attrs, defStyleAttr)
     }
 
-    private fun createFramelayout() {
-//        val frameLayout = FrameLayout(context).apply {
-//            layoutParams = FrameLayout.LayoutParams(
-//                LayoutParams.MATCH_CONSTRAINT,
-//                LayoutParams.MATCH_CONSTRAINT
-//            )
-//            background = ContextCompat.getDrawable(context, R.drawable.duolingo_button_bg_shadow)
-//        }
-//
-//        // Criando as regras de posicionamento no ConstraintLayout
-//        val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_CONSTRAINT).apply {
-//            bottomToBottom = binding.btn.id
-//            startToStart = binding.btn.id
-//            endToEnd = binding.btn.id
-//            startToStart = binding.btn.id
-//        }
-//
-//// Aplicando as regras ao FrameLayout
-//        frameLayout.layoutParams = layoutParams
-//
-//        frameLayout.translationY = dpToPx(4f)
+    private fun inflateLayout() {
+        binding = DuoButtonBinding.inflate(
+            LayoutInflater.from(context), this
+        )
+    }
+
+    private fun bindViews() {
+        button = binding.btn
+        icon = binding.icon
+    }
+
+    private fun loadAttributes(attrs: AttributeSet?, defStyleAttr: Int) {
+        context
+            .theme
+            .obtainStyledAttributes(
+                attrs,
+                R.styleable.DuoButton,
+                defStyleAttr,
+                0
+            )
+            .use {
+                setIconButtonDrawable(it.getDrawable(R.styleable.DuoButton_srcCompat))
+            }
+
+        isClickable = true
+        isFocusable = true
     }
 
     private fun setupOntouchListener() {
         val shadowSize = 2f // dp
         setOnClickListener {  }
         apply {
-            isClickable = true
-            isFocusable = true
             setOnTouchListener { view, event ->
                 when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
@@ -67,17 +76,19 @@ class DuoButton @JvmOverloads constructor(
         }
     }
 
-    private fun bindViews() {
-        button = binding.btn
-    }
-
     fun dpToPx(dp: Float): Float {
         return dp * resources.displayMetrics.density
     }
+}
 
-    private fun inflateLayout() {
-        binding = DuoButtonBinding.inflate(
-            LayoutInflater.from(context), this
-        )
-    }
+@BindingAdapter("srcCompat")
+fun DuoButton.setIconButtonDrawable(icon: Drawable? = null) {
+    this.icon.setImageDrawable(icon)
+    this.icon.isVisible = icon != null
+}
+
+@BindingAdapter("srcCompat")
+fun DuoButton.setIconButtonDrawable(@DrawableRes icon: Int = 0) {
+    this.icon.setImageResource(icon)
+    this.icon.isVisible = icon != 0
 }
